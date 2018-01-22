1860534a41e3a73ef0196651fa89b8b9e408b34d
Steve Yo <s29668518@hotmail.com>
2018-01-22 14:25:44 +0800

    Second commit test

--- app/src/main/java/com/example/steve_yo/test3/MainActivity.java
+++ app/src/main/java/com/example/steve_yo/test3/MainActivity.java
@@ -11,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: ");
+        Log.d(TAG, "onCreate: ");
+    }
+
+    public void test1(){
+        Log.d(TAG, "test1: ");
     }
 }
